4a8b5980d03f48593959493b0fa9b45623f89a93
Audrey Schmidt <audsch@outlook.com>
2022-02-07 14:26:31 -0600

    feat: init of app using create-react-app

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+ReactDOM.render(
+  <Router>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  </Router>,
+  document.getElementById('root')
+);
+
